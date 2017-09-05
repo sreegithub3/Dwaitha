aab431afa656f1b6abb23f77e4c18882077b7f45
Sreenivasa SreekanthR <r.s.sreekanth@gmail.com>
2017-09-05 15:53:44 +0530

    Updated the pom file as well the Jignaasa class

--- src/HomeScreen/Jignaasa.java
+++ src/HomeScreen/Jignaasa.java
@@ -1,6 +1,6 @@
 package HomeScreen;
 
-public class Jignaasa {
+public class Jignaasa{
 
 	public static void main(String[] args) {
 		System.out.println("Hari Sarvottamma...  Vaayu Jeevottamma..");
