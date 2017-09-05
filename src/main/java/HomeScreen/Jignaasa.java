84167e4e319dc738c56458e1319b53035e6f0ddc
Sreenivasa SreekanthR <r.s.sreekanth@gmail.com>
2017-09-05 20:40:17 +0530

    Changes to Jignaasa class

--- src/main/java/HomeScreen/Jignaasa.java
+++ src/main/java/HomeScreen/Jignaasa.java
@@ -5,6 +5,8 @@ public class Jignaasa {
 	public static void main(String[] args) {
 		System.out.println("Hari Sarvottamma...  Vaayu Jeevottamma.. Pancha Bedha.. ** ");
 		System.out.println("Guru Sarvabhama Raghavendra");
+		System.out.println("Sarvan Bharatiya Ramana MukyaPrana Antrgata Sree Krishnaya namah...");
+		
 
 
 	}
