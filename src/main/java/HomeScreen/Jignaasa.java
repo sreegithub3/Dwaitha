acefb6fc6abcffdd786912ab57ad1399d4b6c790
Sreenivasa SreekanthR <r.s.sreekanth@gmail.com>
2017-09-05 16:44:14 +0530

    Updated Jignaasa

--- src/main/java/HomeScreen/Jignaasa.java
+++ src/main/java/HomeScreen/Jignaasa.java
@@ -4,6 +4,7 @@ public class Jignaasa {
 
 	public static void main(String[] args) {
 		System.out.println("Hari Sarvottamma...  Vaayu Jeevottamma.. Pancha Bedha.. ** ");
+		System.out.println("Guru Sarvabhama Raghavendra");
 
 
 	}
