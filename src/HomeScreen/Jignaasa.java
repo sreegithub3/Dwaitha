610ceb99afb24e588f56ddb564a44c47261d19c0
Sreenivasa SreekanthR <r.s.sreekanth@gmail.com>
2017-09-05 15:55:31 +0530

    Pushing all the changes

--- src/HomeScreen/Jignaasa.java
+++ src/HomeScreen/Jignaasa.java
@@ -3,7 +3,7 @@ package HomeScreen;
 public class Jignaasa{
 
 	public static void main(String[] args) {
-		System.out.println("Hari Sarvottamma...  Vaayu Jeevottamma.. Pancha Bedha..");
+		System.out.println("Hari Sarvottamma...  Vaayu Jeevottamma.. Pancha Bedha.. ");
 
 	}
 
